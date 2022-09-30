a3d6d65202dd02c2d1960fd22b42a0644f84217e
rpa10 <rpast@protonmail.com>
2022-09-30 16:28:34 +0200

    text preprocessing & tfidf vectorization

--- parse_vault.py
+++ parse_vault.py
@@ -10,7 +10,8 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 ## Set paths
 cwd = Path.cwd()
-vault_str = '/home/nef/Documents/nefdocs/vault1'
+# vault_str = '/home/nef/Documents/nefdocs/vault1'
+vault_str = r'C:\\Users\\Rafal\\Documents\\vault1'
 vault_pth = Path(vault_str)
 
 ## Parse Obsidian Vault notes
@@ -23,28 +24,108 @@ notes_paths = [
 NOTES = pd.DataFrame(notes_paths, columns=['path'])
 NOTES['contents'] = np.nan
 NOTES['contents'] = NOTES['path'].apply(
-    lambda x: Path(x).read_text()
+    lambda x: Path(x).read_text(encoding='utf-8')
 )
 
-# TODO: get rid of numbers, html code, url links etc. before fit transform vectorizer for TFIDF modeling
-patterns = [
-    "<div style='background-color: #62535D60; padding:5px; border-radius: 5px;'>\n\t<p></p>\n</div>\n\n>", 
-    "div",
-    "Shells",
-    "Tags"
-]
-regs = [
-    '(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])', #match url
-    '(\!\[(.*)\])', #match pictures encoded as ![picturename]
-    '(\$(.*)\$)', #match any LaTex so you can flatten it
-    '(\d\d:\d\d)'#match 00:00 time format to remove it
+NOTES_bak = NOTES.copy()
+
+## PREPROCESSING ##
+
+pats = [
+    # r"<div style='background-color: #62535D60; padding:5px; border-radius: 5px;'>\n\t<p></p>\n</div>\n\n>", 
+    r"shells",
+    r"tags",
+    r"date",
+    r"links",
+    r"source",
+    r"todo",
+    r"\n",
+    r"\t",
+    r'__',
+    r'_',
+    r'(<div.*div>)',
+    r'(#\S+)', #remove tags,
+    r'(\!\[(.*)\])', #remove anything encapsulated in ![ ]
+    # r'(\$(.*)\$)', #match any LaTex so you can flatten it
+    r'(\d\d:\d\d)',#match 00:00 time format to remove it 
+    r'(\d\d\d\d\d\d)', #match date pattern 1
+    r'(\d\d-\d\d-\d\d)', #match dat attern 2
+    r'(\d\d)',
+    r'({.*})' #match special templat expressions
 ]
 
+regurl = [r'(\S+\.(com|net|org|edu|gov|pl|eu|de)(\/\S+)?)'] #match url
+
+# Text preprocessing
+
+NOTES['contents'] = (
+        NOTES['contents']
+        .str.lower()
+)
+
+# Remove known not meaningful patterns
+def sub_patterns(patterns):
+    for pattern in patterns:
+        NOTES['contents'] = (
+            NOTES['contents'].apply(
+                lambda x: re.sub(pattern, ' ', fr'{x}')
+            )
+        )
+sub_patterns(pats)
+
+# Escape special chars so we can match urls
+schars = [
+    ':',
+    '/',
+]
+escape = lambda s, escapechar, specialchars: "".join(
+    escapechar + c if c in specialchars or c == escapechar else c for c in s
+    )
+for chr_ in schars:
+    NOTES['contents'] = (
+        NOTES['contents'].apply(
+            lambda x: escape(x, '\\', chr_)
+        )
+    )
+
+## remove urls now
+sub_patterns(regurl)
+
+
+## test
+cont_test = pd.merge(
+    NOTES['contents'], 
+    NOTES_bak['contents'], 
+    left_index=True, 
+    right_index=True, 
+    how='left'
+)
+
+########################################################
+
 ## Build notes vector representattion table
-text = NOTES.loc[10]['contents'] 
-vectorizer3 = TfidfVectorizer(analyzer='word', ngram_range=(1, 2))
-X3 = vectorizer3.fit_transform([text])
-vectorizer3.get_feature_names_out()
+text = NOTES['contents'] 
+vectorizer = TfidfVectorizer(
+    stop_words='english',
+    max_df=0.99,
+    min_df=0.01,
+    norm='l2',
+    smooth_idf=True,
+    analyzer='word', 
+    ngram_range=(1, 4))
+X = vectorizer.fit_transform(text)
+features = vectorizer.get_feature_names_out()
+
+
+## Construct DF
+FNAMES = NOTES['path'].apply(lambda x: Path(x).stem)
+MDF = pd.DataFrame(
+    X.toarray(),
+    columns=features,
+    index=FNAMES
+)
+
+
 
 
 
